2a7a04c119f967f9efa96d6dd01f00b767746261
jiangming <jqingming@gmail.com>
2015-05-22 23:23:57 +0800

    add bootstrap feature, add navigate bar to index haml.

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,9 @@ module Mingj
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.available_locales = ['en', 'zh_CN']
+    config.i18n.default_locale = :en
+
     config.generators do |g|
       g.test_framework false
     end
