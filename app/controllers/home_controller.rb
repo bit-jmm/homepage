54a091b78655f1540f2d1b76d2251bcd331975f7
jiangming <jqingming@gmail.com>
2015-05-23 19:13:07 +0800

    add favicon, dynamic active class.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if params[:locale].present?
+      flash.now[:locale] = params[:locale]
+    else
+      flash.now[:locale] = 'en'
+    end
   end
 end
